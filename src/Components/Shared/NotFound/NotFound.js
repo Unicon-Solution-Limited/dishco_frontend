95ee71a40f1aaf8b76450305f7fb8f5942b7c9cf
mdmoinulhossain <mdmoinulhossain100@gmail.com>
2023-01-21 12:18:23 +0600

    not found page maked

--- src/Components/Shared/NotFound/NotFound.js
+++ src/Components/Shared/NotFound/NotFound.js
@@ -2,7 +2,15 @@ import React from "react";
 import "./NotFound.css";
 
 const NotFound = () => {
-  return <main>Not Found</main>;
+  return (
+    <main className="not_found_main_body">
+      <h1>404</h1>
+      <h3>Ops! Page Not Found.</h3>
+      <h6>
+        Back To <a href="/">Home</a>
+      </h6>
+    </main>
+  );
 };
 
 export default NotFound;
