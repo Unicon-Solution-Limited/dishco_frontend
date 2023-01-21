48d9d6f2c8e992b4c9cdc8e14e1cb962a2209444
mdmoinulhossain <mdmoinulhossain100@gmail.com>
2023-01-21 17:47:37 +0600

    media queries added

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -5,20 +5,27 @@ import "./Header.css";
 const Header = () => {
   return (
     <main className="header_main_body">
-      <section className="upper_header">
-        <aside className="contact_number">
-          <span>
-            <i class="bi bi-telephone-fill"></i> +880 1810-098389
-          </span>
-          <span>
-            <i class="bi bi-envelope-fill"></i> dishco@uniconbd.com
-          </span>
-        </aside>
-        <aside className="auth_links">
-          <Link>Login</Link>
-          <Link>Sign Up</Link>
-        </aside>
-      </section>
+      <div className="container">
+        <section className="upper_header">
+          <aside className="contact_number">
+            <span>
+              <i class="bi bi-telephone-fill"></i> +880 1810-098389
+            </span>
+            <span>
+              <i class="bi bi-envelope-fill"></i> dishco@uniconbd.com
+            </span>
+          </aside>
+          <aside className="auth_links">
+            <Link className="myLinks">Login</Link> /{" "}
+            <Link className="myLinks">Sign Up</Link>
+          </aside>
+        </section>
+        <section className="lower_header row">
+          <aside className="col-2"></aside>
+          <aside className="col-8"></aside>
+          <aside className="col-2"></aside>
+        </section>
+      </div>
     </main>
   );
 };
