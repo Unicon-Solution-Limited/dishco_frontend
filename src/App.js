8531f60673be3439fafd7ebc343919bf6592a12e
unknown <za0162hid@gmail.com>
2023-01-22 15:59:47 +0600

    router change top added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import NotFound from "./Components/Shared/NotFound/NotFound";
+import RouterChangeTop from "./Components/Shared/RouterChangeTop/RouterChangeTop";
 
 function App() {
   return (
     <>
       <Router>
+        <RouterChangeTop />
         <Switch>
           <Route exact path="/">
             <Home />
