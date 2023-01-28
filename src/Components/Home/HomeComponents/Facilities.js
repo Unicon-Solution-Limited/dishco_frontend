f2e9f86a1a092c523245caadca2d3642c34fe8ff
mdmoinulhossain <mdmoinulhossain100@gmail.com>
2023-01-28 18:04:34 +0600

    aos applied

--- src/Components/Home/HomeComponents/Facilities.js
+++ src/Components/Home/HomeComponents/Facilities.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Facilities = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+  }, []);
   const iCon =
     "https://res.cloudinary.com/dnz6zg4on/image/upload/v1674641182/Frontend_images/Background_images/j49tcxapg63qzbf0sy5d.webp";
   return (
@@ -8,7 +15,7 @@ const Facilities = () => {
       <h2>Facilities</h2>
       <img src={iCon} alt="" />
       <div className="facility_content">
-        <span>Takeaway</span>
+        <span data-aos="fade-right">Takeaway</span>
         <span>Dine In</span>
         <span>Online Order</span>
         <span>Halal Food</span>
