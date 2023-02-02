731d69003a18ecebc7f13ecd67be475fcb3d68df
mdmoinulhossain <mdmoinulhossain100@gmail.com>
2023-02-02 17:11:27 +0600

    page edited

--- src/Components/Shared/NotFound/NotFound.js
+++ src/Components/Shared/NotFound/NotFound.js
@@ -8,7 +8,7 @@ const NotFound = () => {
     <>
       <section className="not_found_main_body">
         <h1>404</h1>
-        <h3>Ops! Page Not Found.</h3>
+        <h3>Page Not Found!</h3>
         <h6>
           Back To <a href="/">Home</a>
         </h6>
