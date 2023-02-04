cd81fadce8c77c882833ab77a71fd4201a865421
mdmoinulhossain <mdmoinulhossain100@gmail.com>
2023-02-04 17:23:34 +0600

    default dashboard removed

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,25 +1,7 @@
 import React from "react";
-import TopbarNav from "./Layouts/TopbarNav/TopbarNav";
-import SidebarNav from "./Layouts/SidebarNav/SidebarNav";
 
 const Dashboard = () => {
-  return (
-    <>
-      <TopbarNav />
-      <div id="layoutSidenav">
-        <SidebarNav />
-        <div id="layoutSidenav_content">
-          <main>
-            <div className="container-fluid p-4">
-              <section className="dashboard_welcome_page">
-                <div>Welcome to Lawyer Profile Admin Dashboard!</div>
-              </section>
-            </div>
-          </main>
-        </div>
-      </div>
-    </>
-  );
+  return <>dashboard</>;
 };
 
 export default Dashboard;
