069abcfe512637dcda89707636cd8f5925655ad1
mdmoinulhossain <mdmoinulhossain100@gmail.com>
2023-02-15 16:58:47 +0600

    privecy policy

--- src/Components/Pages/FooterPages/PrivacyPolicy.js
+++ src/Components/Pages/FooterPages/PrivacyPolicy.js
@@ -3,15 +3,15 @@ import "./FooterPages.css";
 
 const PrivacyPolicy = () => {
   return (
-    <>
-      <h1>Privacy Policy for DishCo Website</h1>
+    <section className="container my-5">
+      <h1 className="text-center">Privacy Policy</h1>
       <p>
         DishCo is committed to protecting the privacy of our customers and
         website visitors. This privacy policy outlines what information we
         collect, how we use it, and your rights regarding that information. By
         accessing our website, you agree to the terms of this privacy policy.
       </p>
-      <h2>Information We Collect</h2>
+      <h2 className="mt-5">Information We Collect</h2>
       <p>
         We collect two types of information through our website: personal
         information and non-personal information.
@@ -28,7 +28,7 @@ const PrivacyPolicy = () => {
         this information automatically when you visit our website through the
         use of cookies and other tracking technologies.
       </p>
-      <h2>How We Use Your Information</h2>
+      <h2 className="mt-5">How We Use Your Information</h2>
       <p>
         We use the information we collect for a variety of purposes, including:
       </p>
@@ -43,7 +43,7 @@ const PrivacyPolicy = () => {
         We may also use your information to comply with legal requirements, such
         as responding to a subpoena or court order.
       </p>
-      <h2>Sharing Your Information</h2>
+      <h2 className="mt-5">Sharing Your Information</h2>
       <p>
         We do not sell or rent your personal information to third parties.
         However, we may share your information with our affiliated companies or
@@ -56,13 +56,13 @@ const PrivacyPolicy = () => {
         legal request, such as a subpoena or court order, or in the event of a
         merger, acquisition, or other corporate change.
       </p>
-      <h2>Your Rights</h2>
+      <h2 className="mt-5">Your Rights</h2>
       <p>
         You have the right to access, modify, and delete your personal
         information. You can also opt out of receiving promotional emails from
         us by clicking the "unsubscribe" link in the email.
       </p>
-      <h2>Security</h2>
+      <h2 className="mt-5">Security</h2>
       <p>
         We take the security of your information very seriously and have
         implemented appropriate measures to protect it from unauthorized access,
@@ -70,14 +70,14 @@ const PrivacyPolicy = () => {
         completely secure, and we cannot guarantee the security of your
         information.
       </p>
-      <h2>Children's Privacy</h2>
+      <h2 className="mt-5">Children's Privacy</h2>
       <p>
         Our website is not intended for children under the age of 13, and we do
         not knowingly collect personal information from children. If you are a
         parent or guardian and believe we have collected personal information
         from a child under the
       </p>{" "}
-    </>
+    </section>
   );
 };
 
