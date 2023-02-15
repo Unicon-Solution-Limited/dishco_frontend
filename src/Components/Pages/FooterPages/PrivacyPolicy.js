2b113767d82f9ecc0aa53c40109680b3e65fea7e
mdmoinulhossain <mdmoinulhossain100@gmail.com>
2023-02-15 18:26:13 +0600

    update

--- src/Components/Pages/FooterPages/PrivacyPolicy.js
+++ src/Components/Pages/FooterPages/PrivacyPolicy.js
@@ -1,83 +1,94 @@
 import React from "react";
 import "./FooterPages.css";
+import Header from "./../../Shared/Header/Header";
+import Footer from "./../../Shared/Footer/Footer";
 
 const PrivacyPolicy = () => {
   return (
-    <section className="container my-5">
-      <h1 className="text-center">Privacy Policy</h1>
-      <p>
-        DishCo is committed to protecting the privacy of our customers and
-        website visitors. This privacy policy outlines what information we
-        collect, how we use it, and your rights regarding that information. By
-        accessing our website, you agree to the terms of this privacy policy.
-      </p>
-      <h2 className="mt-5">Information We Collect</h2>
-      <p>
-        We collect two types of information through our website: personal
-        information and non-personal information.
-      </p>
-      <p>
-        Personal information is information that can be used to identify you,
-        such as your name, address, email address, and telephone number. We
-        collect this information when you make a reservation, place an order,
-        sign up for our email list, or participate in a survey or contest.
-      </p>
-      <p>
-        Non-personal information is information that cannot be used to identify
-        you, such as your IP address, browser type, and location. We collect
-        this information automatically when you visit our website through the
-        use of cookies and other tracking technologies.
-      </p>
-      <h2 className="mt-5">How We Use Your Information</h2>
-      <p>
-        We use the information we collect for a variety of purposes, including:
-      </p>
-      <ul>
-        <li>To fulfill your order or reservation</li>
-        <li>To send you promotional emails and special offers</li>
-        <li>To improve our website and services</li>
-        <li>To conduct market research</li>
-        <li>To provide customer support</li>
-      </ul>
-      <p>
-        We may also use your information to comply with legal requirements, such
-        as responding to a subpoena or court order.
-      </p>
-      <h2 className="mt-5">Sharing Your Information</h2>
-      <p>
-        We do not sell or rent your personal information to third parties.
-        However, we may share your information with our affiliated companies or
-        service providers for the purposes described above. These companies are
-        contractually obligated to protect your information and are not
-        permitted to use it for any other purpose.
-      </p>
-      <p>
-        We may also be required to disclose your information in response to a
-        legal request, such as a subpoena or court order, or in the event of a
-        merger, acquisition, or other corporate change.
-      </p>
-      <h2 className="mt-5">Your Rights</h2>
-      <p>
-        You have the right to access, modify, and delete your personal
-        information. You can also opt out of receiving promotional emails from
-        us by clicking the "unsubscribe" link in the email.
-      </p>
-      <h2 className="mt-5">Security</h2>
-      <p>
-        We take the security of your information very seriously and have
-        implemented appropriate measures to protect it from unauthorized access,
-        use, or disclosure. However, no website or internet transmission is
-        completely secure, and we cannot guarantee the security of your
-        information.
-      </p>
-      <h2 className="mt-5">Children's Privacy</h2>
-      <p>
-        Our website is not intended for children under the age of 13, and we do
-        not knowingly collect personal information from children. If you are a
-        parent or guardian and believe we have collected personal information
-        from a child under the
-      </p>{" "}
-    </section>
+    <>
+      <Header />
+      <section className="footerPage_basic privacy_policy">
+        <div className="container">
+          <h1 className="text-center">Privacy Policy</h1>
+          <p>
+            DishCo is committed to protecting the privacy of our customers and
+            website visitors. This privacy policy outlines what information we
+            collect, how we use it, and your rights regarding that information.
+            By accessing our website, you agree to the terms of this privacy
+            policy.
+          </p>
+          <h2 className="mt-5">Information We Collect</h2>
+          <p>
+            We collect two types of information through our website: personal
+            information and non-personal information.
+          </p>
+          <p>
+            Personal information is information that can be used to identify
+            you, such as your name, address, email address, and telephone
+            number. We collect this information when you make a reservation,
+            place an order, sign up for our email list, or participate in a
+            survey or contest.
+          </p>
+          <p>
+            Non-personal information is information that cannot be used to
+            identify you, such as your IP address, browser type, and location.
+            We collect this information automatically when you visit our website
+            through the use of cookies and other tracking technologies.
+          </p>
+          <h2 className="mt-5">How We Use Your Information</h2>
+          <p>
+            We use the information we collect for a variety of purposes,
+            including:
+          </p>
+          <ul>
+            <li>To fulfill your order or reservation</li>
+            <li>To send you promotional emails and special offers</li>
+            <li>To improve our website and services</li>
+            <li>To conduct market research</li>
+            <li>To provide customer support</li>
+          </ul>
+          <p>
+            We may also use your information to comply with legal requirements,
+            such as responding to a subpoena or court order.
+          </p>
+          <h2 className="mt-5">Sharing Your Information</h2>
+          <p>
+            We do not sell or rent your personal information to third parties.
+            However, we may share your information with our affiliated companies
+            or service providers for the purposes described above. These
+            companies are contractually obligated to protect your information
+            and are not permitted to use it for any other purpose.
+          </p>
+          <p>
+            We may also be required to disclose your information in response to
+            a legal request, such as a subpoena or court order, or in the event
+            of a merger, acquisition, or other corporate change.
+          </p>
+          <h2 className="mt-5">Your Rights</h2>
+          <p>
+            You have the right to access, modify, and delete your personal
+            information. You can also opt out of receiving promotional emails
+            from us by clicking the "unsubscribe" link in the email.
+          </p>
+          <h2 className="mt-5">Security</h2>
+          <p>
+            We take the security of your information very seriously and have
+            implemented appropriate measures to protect it from unauthorized
+            access, use, or disclosure. However, no website or internet
+            transmission is completely secure, and we cannot guarantee the
+            security of your information.
+          </p>
+          <h2 className="mt-5">Children's Privacy</h2>
+          <p>
+            Our website is not intended for children under the age of 13, and we
+            do not knowingly collect personal information from children. If you
+            are a parent or guardian and believe we have collected personal
+            information from a child under the
+          </p>{" "}
+        </div>
+      </section>
+      <Footer />
+    </>
   );
 };
 
