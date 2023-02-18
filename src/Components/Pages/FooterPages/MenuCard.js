1f53b45ee19a0838f98c20c76b33e4d418e4c363
mdmoinulhossain <mdmoinulhossain100@gmail.com>
2023-02-18 18:31:14 +0600

    menu card added

--- src/Components/Pages/FooterPages/MenuCard.js
+++ src/Components/Pages/FooterPages/MenuCard.js
@@ -4,10 +4,143 @@ import Header from "../../Shared/Header/Header";
 import "./FooterPages.css";
 
 const MenuCard = () => {
+  const m1 =
+    "https://res.cloudinary.com/dnz6zg4on/image/upload/v1676721660/Frontend_images/menus/gw8zpwqmzb2odme6hmtp.webp";
+  const m2 =
+    "https://res.cloudinary.com/dnz6zg4on/image/upload/v1676721660/Frontend_images/menus/rwztpletpbt94cqqame6.webp";
+  const m3 =
+    "https://res.cloudinary.com/dnz6zg4on/image/upload/v1676721659/Frontend_images/menus/ym5klsvquogeeupsakhw.webp";
+  const m4 =
+    "https://res.cloudinary.com/dnz6zg4on/image/upload/v1676721659/Frontend_images/menus/relpqway0aqtma1uedvg.webp";
+  const m5 =
+    "https://res.cloudinary.com/dnz6zg4on/image/upload/v1676721659/Frontend_images/menus/nb4zrmf7reup1wpyja1n.webp";
+  const m6 =
+    "https://res.cloudinary.com/dnz6zg4on/image/upload/v1676721660/Frontend_images/menus/gw8zpwqmzb2odme6hmtp.webp";
+  const m7 =
+    "https://res.cloudinary.com/dnz6zg4on/image/upload/v1676721660/Frontend_images/menus/gw8zpwqmzb2odme6hmtp.webp";
+  const m8 =
+    "https://res.cloudinary.com/dnz6zg4on/image/upload/v1676721660/Frontend_images/menus/gw8zpwqmzb2odme6hmtp.webp";
+  const m9 =
+    "https://res.cloudinary.com/dnz6zg4on/image/upload/v1676721660/Frontend_images/menus/gw8zpwqmzb2odme6hmtp.webp";
+  const m10 =
+    "https://res.cloudinary.com/dnz6zg4on/image/upload/v1676721660/Frontend_images/menus/gw8zpwqmzb2odme6hmtp.webp";
+  const m11 =
+    "https://res.cloudinary.com/dnz6zg4on/image/upload/v1676721660/Frontend_images/menus/gw8zpwqmzb2odme6hmtp.webp";
+  const m12 =
+    "https://res.cloudinary.com/dnz6zg4on/image/upload/v1676721660/Frontend_images/menus/gw8zpwqmzb2odme6hmtp.webp";
+  const m13 =
+    "https://res.cloudinary.com/dnz6zg4on/image/upload/v1676721660/Frontend_images/menus/gw8zpwqmzb2odme6hmtp.webp";
+  const m14 =
+    "https://res.cloudinary.com/dnz6zg4on/image/upload/v1676721660/Frontend_images/menus/gw8zpwqmzb2odme6hmtp.webp";
+  const m15 =
+    "https://res.cloudinary.com/dnz6zg4on/image/upload/v1676721660/Frontend_images/menus/gw8zpwqmzb2odme6hmtp.webp";
+  const m16 =
+    "https://res.cloudinary.com/dnz6zg4on/image/upload/v1676721660/Frontend_images/menus/gw8zpwqmzb2odme6hmtp.webp";
+  const m17 =
+    "https://res.cloudinary.com/dnz6zg4on/image/upload/v1676721660/Frontend_images/menus/gw8zpwqmzb2odme6hmtp.webp";
+  const m18 =
+    "https://res.cloudinary.com/dnz6zg4on/image/upload/v1676721660/Frontend_images/menus/gw8zpwqmzb2odme6hmtp.webp";
   return (
     <>
       <Header />
-      Menu Card
+      <div id="carouselExampleIndicators" class="carousel slide">
+        <div class="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="0"
+            class="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src={m1} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src={m2} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src={m3} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src={m4} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src={m5} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src={m6} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src={m7} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src={m8} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src={m9} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src={m10} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src={m11} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src={m12} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src={m13} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src={m14} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src={m15} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src={m16} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src={m17} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src={m18} class="d-block w-100" alt="..." />
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
       <Footer />
     </>
   );
