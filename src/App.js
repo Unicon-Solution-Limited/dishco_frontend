4a076fc6b627992c8abf6094e2baecde18b68a51
unknown <za0162hid@gmail.com>
2023-02-20 16:39:50 +0600

    system added

--- src/App.js
+++ src/App.js
@@ -21,81 +21,84 @@ import PrivacyPolicy from "./Components/Pages/FooterPages/PrivacyPolicy";
 import Returnpolicy from "./Components/Pages/FooterPages/Returnpolicy";
 import Shop from "./Components/Pages/FooterPages/Shop";
 import TandC from "./Components/Pages/FooterPages/TandC";
+import CartContext from "./Components/AllContext/CartContext";
 
 function App() {
   return (
     <>
-      <Router>
-        <RouterChangeTop />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
+      <CartContext>
+        <Router>
+          <RouterChangeTop />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
 
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
 
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/signup">
-            <Signup />
-          </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
 
-          {/* Dashboard router start */}
-          <Route path="/createProduct">
-            <CreateProduct />
-          </Route>
-          <Route path="/allProduct">
-            <AllProduct />
-          </Route>
-          {/* Dashboard router end */}
+            {/* Dashboard router start */}
+            <Route path="/createProduct">
+              <CreateProduct />
+            </Route>
+            <Route path="/allProduct">
+              <AllProduct />
+            </Route>
+            {/* Dashboard router end */}
 
-          <Route path="/forgotPassword">
-            <ForgotPassword />
-          </Route>
-          <Route path="/products/:navItem">
-            <DisplayProduct />
-          </Route>
-          <Route path="/singleProduct/:viewDetails">
-            <SingleProduct />
-          </Route>
+            <Route path="/forgotPassword">
+              <ForgotPassword />
+            </Route>
+            <Route path="/products/:navItem">
+              <DisplayProduct />
+            </Route>
+            <Route path="/singleProduct/:viewDetails">
+              <SingleProduct />
+            </Route>
 
-          <Route path="/cart">
-            <Cart />
-          </Route>
-          <Route path="/checkout">
-            <Checkout />
-          </Route>
-          {/* Footer Pages Router */}
-          <Route path="/about-us">
-            <AboutUs />
-          </Route>
-          <Route path="/contact-us">
-            <ContactUs />
-          </Route>
-          <Route path="/menus">
-            <MenuCard />
-          </Route>
-          <Route path="/privacy-policy">
-            <PrivacyPolicy />
-          </Route>
-          <Route path="/return-policy">
-            <Returnpolicy />
-          </Route>
-          <Route path="/shop">
-            <Shop />
-          </Route>
-          <Route path="/t_and_c">
-            <TandC />
-          </Route>
-          {/* Not Found Page */}
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
-      </Router>
+            <Route path="/cart">
+              <Cart />
+            </Route>
+            <Route path="/checkout">
+              <Checkout />
+            </Route>
+            {/* Footer Pages Router */}
+            <Route path="/about-us">
+              <AboutUs />
+            </Route>
+            <Route path="/contact-us">
+              <ContactUs />
+            </Route>
+            <Route path="/menus">
+              <MenuCard />
+            </Route>
+            <Route path="/privacy-policy">
+              <PrivacyPolicy />
+            </Route>
+            <Route path="/return-policy">
+              <Returnpolicy />
+            </Route>
+            <Route path="/shop">
+              <Shop />
+            </Route>
+            <Route path="/tc">
+              <TandC />
+            </Route>
+            {/* Not Found Page */}
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </Router>
+      </CartContext>
     </>
   );
 }
