d317e67d5cb9fd70b83713a3a25f9891ab9876ae
unknown <za0162hid@gmail.com>
2023-02-23 16:57:15 +0600

    addon added

--- src/Components/Shared/Header/HeaderOffcanvas.js
+++ src/Components/Shared/Header/HeaderOffcanvas.js
@@ -4,9 +4,47 @@ import { CartProvider } from "../../AllContext/CartContext";
 
 const HeaderOffcanvas = () => {
   const [cartData, setCartData] = useContext(CartProvider);
-  console.log(cartData);
-  const testImg =
-    "https://res.cloudinary.com/dnz6zg4on/image/upload/v1674643571/Frontend_images/Background_images/ah3nx1cd824n7wr2vx4n.webp";
+
+  console.log(cartData, "cartData");
+  console.log(cartData?.extras);
+
+  //total addonPrice
+  // const totalAddonPrice = cartData.map((cartDt) =>
+  //   cartDt?.extras.reduce((accumulator, addon) => {
+  //     return accumulator + addon.priceOfAddon;
+  //   }, 0)
+  // );
+  // const FinaltotalAddonPrice = totalAddonPrice.reduce(
+  //   (accumulator, currentValue) => {
+  //     return accumulator + currentValue;
+  //   },
+  //   0
+  // );
+  const finaltotalAddonPrice = cartData?.reduce((accumulator, cartDt) => {
+    const addonsPrice = cartDt?.extras?.reduce((accumulator, addon) => {
+      return accumulator + addon.priceOfAddon;
+    }, 0);
+    return accumulator + addonsPrice;
+  }, 0);
+
+  console.log(finaltotalAddonPrice, "price");
+
+  // Calculate the total price of all items in the cart
+  // const totalPrice = cartData.reduce(
+  //   (acc, item) => acc + item.quantity * item.price,
+  //   0
+  // );
+
+  // Add the total addon price to the subtotal
+  // const subtotal = totalPrice + totalAddonPrice;
+
+  // Delete items from cart
+  const deleteItem = (id) => {
+    console.log(id);
+    let newData = cartData.filter((item) => item.id !== id);
+    setCartData(newData);
+  };
+
   return (
     <div
       className="offcanvas offcanvas-end"
@@ -26,25 +64,33 @@ const HeaderOffcanvas = () => {
         ></button>
       </div>
       <div className="offcanvas-body">
-        <div className="offcanvas_card_single_item">
-          <img src={testImg} alt="" className="offcanvas_cart_img" />
-          <span>
-            <p className="offcanvas_cart_product_name">
-              DishCo Special Ramen Items
-            </p>
-            <p className="offcanvas_cart_price_qnt">
-              <span>2</span>x <span>350 tk.</span>
-              <span>= 700 tk.</span>
-            </p>
-          </span>
-          <span className="offcanvas_cancellation">
-            <button className="btn">
-              <i className="bi bi-x-circle"></i>
-            </button>
-          </span>
-        </div>
+        {cartData.map((cartDt) => (
+          <div className="offcanvas_card_single_item" key={cartDt?._id}>
+            <img src={cartDt?.image} alt="" className="offcanvas_cart_img" />
+            <span>
+              <p className="offcanvas_cart_product_name">{cartDt?.name}</p>
+              <p className="offcanvas_cart_price_qnt">
+                <span>{cartDt?.quantity}</span>x
+                <span>
+                  {cartDt?.price} tk. +{" "}
+                  {cartDt?.extras.reduce(
+                    (acc, addon) => acc + addon.priceOfAddon,
+                    0
+                  )}
+                  tk.
+                </span>
+                <span>= 0</span>
+              </p>
+            </span>
+            <span className="offcanvas_cancellation">
+              <button className="btn" onClick={() => deleteItem(cartDt?.id)}>
+                <i className="bi bi-x-circle"></i>
+              </button>
+            </span>
+          </div>
+        ))}
         <div className="offcanvas_cart_footer">
-          <h4>Subtotal: 1030 Tk.</h4>
+          {/* <h4>Subtotal: {subtotal} Tk.</h4> */}
           <span className="offcanvas_cart_buttons">
             <button className="btn" data-bs-dismiss="offcanvas">
               <Link to="/cart" className="MyBtn offcanvas_cart_button">
