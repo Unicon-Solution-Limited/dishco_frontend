d74744754a6e5dec3e29946b8bb6f042f9930413
unknown <za0162hid@gmail.com>
2023-03-05 16:14:30 +0600

    customer order function

--- src/Components/Dashboard/DashboardPage/OrderManagement/CustomerOrder.js
+++ src/Components/Dashboard/DashboardPage/OrderManagement/CustomerOrder.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./OrderManagement.css";
 import TopbarNav from "./../../Layouts/TopbarNav";
 import SidebarNav from "./../../Layouts/SidebarNav";
+import axios from "axios";
+import { useAuth } from "../../../Authentication/AuthContext/AuthContext";
 
 const CustomerOrder = () => {
+  const { currentUser } = useAuth();
+  const [customerOders, setCustomerOrders] = useState([]);
+  console.log(customerOders);
+  useEffect(() => {
+    const fetchCustomerOrders = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8000/getCustomerOrders?email=${currentUser.email}`
+        );
+        setCustomerOrders(response?.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchCustomerOrders();
+  }, [currentUser.email]);
   const demo =
     "https://res.cloudinary.com/dnz6zg4on/image/upload/v1674643571/Frontend_images/Background_images/ah3nx1cd824n7wr2vx4n.webp";
   return (
