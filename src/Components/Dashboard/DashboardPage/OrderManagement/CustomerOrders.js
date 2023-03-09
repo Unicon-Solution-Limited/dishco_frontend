b896ad33596cd324292d5f751b7d7617b16812f8
unknown <za0162hid@gmail.com>
2023-03-09 18:17:10 +0600

    customer orders

--- src/Components/Dashboard/DashboardPage/OrderManagement/CustomerOrders.js
+++ src/Components/Dashboard/DashboardPage/OrderManagement/CustomerOrders.js
@@ -8,23 +8,38 @@ import PopupOrderCustomer from "./PopupOrderCustomer";
 
 const CustomerOrders = () => {
   const { currentUser } = useAuth();
-  const [customerOders, setCustomerOrders] = useState([]);
-  console.log(customerOders);
+  const [allCustomerOders, setallCustomerOrders] = useState([]);
+  const [customerOrderDetails, setCustomerOrderDetails] = useState([]);
+
+  //getting the customer order according the email
   useEffect(() => {
     const fetchCustomerOrders = async () => {
-      try {
-        const response = await axios.get(
-          `http://localhost:8000/getCustomerOrders?email=${currentUser.email}`
-        );
-        setCustomerOrders(response?.data);
-      } catch (error) {
-        console.log(error);
+      if (currentUser) {
+        try {
+          const response = await axios.get(
+            `http://localhost:8000/getCustomerOrders?email=${currentUser.email}`
+          );
+          setallCustomerOrders(response?.data);
+        } catch (error) {
+          console.log(error);
+        }
       }
     };
     fetchCustomerOrders();
-  }, [currentUser.email]);
-  const demo =
-    "https://res.cloudinary.com/dnz6zg4on/image/upload/v1674643571/Frontend_images/Background_images/ah3nx1cd824n7wr2vx4n.webp";
+  }, [currentUser]);
+
+  //customer order data details according to the train id
+  const handleCustomerTranId = async (tran_id) => {
+    await axios
+      .get(`http://localhost:8000/customerOrderDetails?tran_id=` + tran_id)
+      .then((response) => {
+        setCustomerOrderDetails(response?.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <>
       <TopbarNav />
@@ -33,18 +48,20 @@ const CustomerOrders = () => {
         <div id="layoutSidenav_content">
           <main className="customer_order">
             <h3 className="mb-5">Your Order List's per Date</h3>
-
-            <button
-              className="btn single_order_section"
-              data-bs-toggle="modal"
-              data-bs-target="#singleOrderModal"
-            >
-              05.03.2023 <br />
-              <strong>Order ID:</strong> 4587-8Ac9
-            </button>
+            {allCustomerOders.map((allCustomerOrder) => (
+              <button
+                className="btn single_order_section m-2"
+                data-bs-toggle="modal"
+                data-bs-target="#singleOrderModal"
+                key={allCustomerOrder._id}
+                onClick={() => handleCustomerTranId(allCustomerOrder?.tran_id)}
+              >
+                {allCustomerOrder?.orderTime} <br />
+                <strong>Order ID:</strong> {allCustomerOrder?.tran_id}
+              </button>
+            ))}
+            <PopupOrderCustomer customerOrderDetails={customerOrderDetails} />
           </main>
-
-          <PopupOrderCustomer />
         </div>
       </div>
     </>
--- src/Components/Dashboard/DashboardPage/OrderManagement/PopupOrderCustomer.js
+++ src/Components/Dashboard/DashboardPage/OrderManagement/PopupOrderCustomer.js
@@ -1,91 +1,94 @@
 import React from "react";
 import "./OrderManagement.css";
 
-const PopupOrderCustomer = () => {
+const PopupOrderCustomer = ({ customerOrderDetails }) => {
+  console.log(customerOrderDetails);
   const demo =
     "https://res.cloudinary.com/dnz6zg4on/image/upload/v1674643571/Frontend_images/Background_images/ah3nx1cd824n7wr2vx4n.webp";
   return (
-    <div
-      className="modal fade"
-      id="singleOrderModal"
-      tabIndex="-1"
-      aria-labelledby="singleOrderModalLabel"
-      aria-hidden="true"
-    >
-      <div className="modal-dialog modal-dialog-scrollable modal-lg">
-        <div className="modal-content">
-          <div className="modal-header">
-            <aside className="order_date_id">
-              <p>
-                <strong>Date & Time:</strong> 04.03.2023 5:30 PM
-              </p>
-              <p>
-                <strong>Order Id:</strong> dJfv-05BG
-              </p>
-            </aside>
-            <button
-              type="button"
-              className="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div className="modal-body">
-            <div className="product_single_order_items">
-              <section className="product_single_order_first_col">
-                <aside className="my-2">
-                  <img src={demo} alt="" className="order_image" />
-                </aside>
+    <>
+      <div
+        className="modal fade"
+        id="singleOrderModal"
+        tabIndex="-1"
+        aria-labelledby="singleOrderModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-scrollable modal-lg">
+          <div className="modal-content">
+            <div className="modal-header">
+              <aside className="order_date_id">
+                <p>
+                  <strong>Date & Time:</strong> 04.03.2023 5:30 PM
+                </p>
+                <p>
+                  <strong>Order Id:</strong> dJfv-05BG
+                </p>
+              </aside>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <div className="product_single_order_items">
+                <section className="product_single_order_first_col">
+                  <aside className="my-2">
+                    <img src={demo} alt="" className="order_image" />
+                  </aside>
+                  <aside className="order_info">
+                    <h5>DishCo Special Platter</h5>
+                    <p>
+                      <strong>Price:</strong> 600 tk.
+                    </p>
+                    <p>
+                      <strong>Quantity:</strong> 2
+                    </p>
+                  </aside>
+                </section>
                 <aside className="order_info">
-                  <h5>DishCo Special Platter</h5>
-                  <p>
-                    <strong>Price:</strong> 600 tk.
-                  </p>
-                  <p>
-                    <strong>Quantity:</strong> 2
+                  <span>
+                    <strong>Extra Items:</strong>
+                    <li>Sause 30 tk.</li>
+                    <li>Mewonese 30 tk.</li>
+                  </span>
+                  <p className="mt-3">
+                    <strong>Size(If Any):</strong> 8"
                   </p>
                 </aside>
-              </section>
-              <aside className="order_info">
-                <span>
-                  <strong>Extra Items:</strong>
-                  <li>Sause 30 tk.</li>
-                  <li>Mewonese 30 tk.</li>
-                </span>
-                <p className="mt-3">
-                  <strong>Size(If Any):</strong> 8"
+              </div>
+              <aside className="discount_delivery_charge">
+                <p>
+                  <strong>Delivery Charge:</strong> 120 tk.
+                </p>
+                <p>
+                  <strong>Discount:</strong> 120 tk.
+                </p>
+                <p>
+                  <strong>Total:</strong> 600 tk.
                 </p>
               </aside>
             </div>
-            <aside className="discount_delivery_charge">
+            <div className="modal-footer single_order_modal_footer">
               <p>
-                <strong>Delivery Charge:</strong> 120 tk.
+                <strong>Status:</strong>
+                <span className="status pending">Pending</span>
               </p>
-              <p>
-                <strong>Discount:</strong> 120 tk.
-              </p>
-              <p>
-                <strong>Total:</strong> 600 tk.
-              </p>
-            </aside>
-          </div>
-          <div className="modal-footer single_order_modal_footer">
-            <p>
-              <strong>Status:</strong>
-              <span className="status pending">Pending</span>
-            </p>
-            <aside className="order_cancel">
-              <button className="btn invoice_btn">Invoice</button>
-            </aside>
-            <aside className="order_cancel MyBtn">
-              <button className="btn">
-                Cancel order <i className="bi bi-trash"></i>
-              </button>
-            </aside>
+              <aside className="order_cancel">
+                <button className="btn invoice_btn">Invoice</button>
+              </aside>
+              <aside className="order_cancel MyBtn">
+                <button className="btn">
+                  Cancel order <i className="bi bi-trash"></i>
+                </button>
+              </aside>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
