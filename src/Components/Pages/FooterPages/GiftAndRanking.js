f95f0878b1863df355afe9e70ab51ece0b73d917
mdmoinulhossain <mdmoinulhossain100@gmail.com>
2023-03-28 11:58:11 +0600

    rank display update

--- src/Components/Pages/FooterPages/GiftAndRanking.js
+++ src/Components/Pages/FooterPages/GiftAndRanking.js
@@ -11,20 +11,36 @@ const GiftAndRanking = () => {
         <section className="container pt-5">
           <h2 className="text-center rank_headline">Customer Ranking</h2>
           <div className="rank_body_main py-5">
-            <div className="rank_body bronze">
-              <p>Bronze</p>
+            <div className="rank_body">
+              <img
+                src="https://res.cloudinary.com/dnz6zg4on/image/upload/v1679982155/Frontend_images/logo/caj3pktvujd45qvbjm61.webp"
+                alt="Bronze"
+                className="logo_for_rank"
+              />
               <p>0 - 499 Points</p>
             </div>
-            <div className="rank_body silver">
-              <p>Silver</p>
+            <div className="rank_body">
+              <img
+                src="https://res.cloudinary.com/dnz6zg4on/image/upload/v1679982155/Frontend_images/logo/ezbuvfohme2cdiiphgiu.webp"
+                alt="Silver"
+                className="logo_for_rank"
+              />
               <p>500 - 2999 Points</p>
             </div>
-            <div className="rank_body gold">
-              <p>Gold</p>
+            <div className="rank_body">
+              <img
+                src="https://res.cloudinary.com/dnz6zg4on/image/upload/v1679982155/Frontend_images/logo/bzczji4nlplgwwjud8bi.webp"
+                alt="Gold"
+                className="logo_for_rank"
+              />
               <p>3000 - 6999 Points</p>
             </div>
-            <div className="rank_body platinum">
-              <p>Platinum</p>
+            <div className="rank_body">
+              <img
+                src="https://res.cloudinary.com/dnz6zg4on/image/upload/v1679982155/Frontend_images/logo/wkogtc6hr1r8pwhnfcnr.webp"
+                alt="Platinum"
+                className="logo_for_rank"
+              />
               <p>
                 7000 - <i className="bi bi-infinity"></i> Points
               </p>
