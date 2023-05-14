d1c69b23f0da930aa401e253e3a221422adb76ad
MD. MOINUL Hossian <mdmoinulhossain100@gmail.com>
2023-05-14 16:43:33 +0600

    credentials replace

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyAL7kAdnVItOYFr4o6rCQ-p3bmtLZEfcY8",
-  authDomain: "dishco-c3698.firebaseapp.com",
-  projectId: "dishco-c3698",
-  storageBucket: "dishco-c3698.appspot.com",
-  messagingSenderId: "1074663610277",
-  appId: "1:1074663610277:web:c98576cc14a3d994103a4c",
-  measurementId: "G-X22NDNJE64",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 });
 
 export const provider = new firebase.auth.GoogleAuthProvider();
