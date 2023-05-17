39f6ed459d42d743c182d49b4f6c93899490ae36
unknown <za0162hid@gmail.com>
2023-05-17 13:09:29 +0600

    number added

--- src/Components/Pages/Checkout/Success.js
+++ src/Components/Pages/Checkout/Success.js
@@ -21,11 +21,9 @@ const Success = () => {
           <i className="bi bi-check-circle"></i>
         </span>
         <p className="mt-2">Thank you for your purchase.</p>
-        <strong>Your Order ID is: FVBG458741236</strong>
-        <p>
-          You will received an order confirmation email with details of your
-          order.
-        </p>
+
+        <strong className="mt-2">Please call to confirm your order.</strong>
+        <h3>+88 01810-098389</h3>
         <div className="success_redirect_button mt-4">
           <Link to="/" className="btn MyBtn">
             Continue Shopping
