919fc50d05738b23f6f20f9c9c1f7e174ac58fe2
mdmoinulhossain <mdmoinulhossain100@gmail.com>
2023-08-23 17:50:14 +0600

    catering checkout page edited

--- src/Components/Pages/CateringService/CateringCheckoutPage.js
+++ src/Components/Pages/CateringService/CateringCheckoutPage.js
@@ -2,6 +2,8 @@ import React, { useEffect, useRef } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { useAuth } from "../../Authentication/AuthContext/AuthContext";
 import { useState } from "react";
+import Footer from "./../../Shared/Footer/Footer";
+import Header from "./../../Shared/Header/Header";
 
 const CateringCheckoutPage = () => {
   const history = useHistory();
@@ -86,171 +88,208 @@ const CateringCheckoutPage = () => {
 
   return (
     <>
-      <form className="checkout_body container" onSubmit={handleConfirmOrder}>
-        <aside className="billing_form">
-          <h5>Shipping Address</h5>
-          <div className="mb-3">
-            <label htmlFor="inputName" className="form-label mandatory_field">
-              Your name
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="inputName"
-              defaultValue={currentUser?.displayName}
-              readOnly
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="area" className="form-label mandatory_field">
-              Area
-            </label>
-            <select
-              className="form-select"
-              aria-label="Default select example"
-              ref={cityRef}
-              required
-            >
-              <option value="">Select Your Area</option>
-              <option value="Azimpur">Azimpur</option>
-              <option value="Chankharpul">Chankharpul</option>
-              <option value="Dhaka University Hall">
-                Dhaka University Hall
-              </option>
-              <option value="Darussalam">Darussalam</option>
-              <option value="Elephent Road">Elephent Road</option>
-              <option value="Farmgate">Farmgate</option>
-              <option value="Gulistan">Gulistan</option>
-              <option value="Green Road">Green Road</option>
-              <option value="Kola Bagan">Kola Bagan</option>
-              <option value="Kamrangirchar">Kamrangirchar</option>
-              <option value="Kallyanpur">Kallyanpur</option>
-              <option value="Mohammadpur Bus Stand">
-                Mohammadpur Bus Stand
-              </option>
-              <option value="Mohammadpur beribadh">Mohammadpur beribadh</option>
-              <option value="Mohammadpur Adabor">Mohammadpur Adabor</option>
-              <option value="Mohammadpur Asad Gate">
-                Mohammadpur Asad Gate
-              </option>
-              <option value="Mirpur-1">Mirpur-1</option>
-              <option value="New Market, Dhaka">New Market, Dhaka</option>
-              <option value="Nakhalpara West Tejgaon">
-                Nakhalpara West Tejgaon
-              </option>
-              <option value="Nakhalpara south">Nakhalpara south</option>
-              <option value="Shahbag">Shahbag</option>
-              <option value="Shyamoli-1">Shyamoli-1</option>
-              <option value="Shyamoli-2">Shyamoli-2</option>
-              <option value="Panthopath Chattor">Panthopath Chattor</option>
-              <option value="Polashi">Polashi</option>
-              <option value="Puran Dhaka(Bangshal)">
-                Puran Dhaka(Bangshal)
-              </option>
-              <option value="Puran Dhaka (Dholaikhal)">
-                Puran Dhaka (Dholaikhal)
-              </option>
-              <option value="Rayer Bazaar">Rayer Bazaar</option>
-            </select>
-          </div>
-          <div className="mb-3">
-            <label
-              htmlFor="inputAddress"
-              className="form-label mandatory_field"
-            >
-              Details Address
-            </label>
-            <input
-              ref={addressRef}
-              type="text"
-              className="form-control"
-              id="inputAddress"
-              required
-            />
-          </div>
-
-          <div className="mb-3">
-            <label
-              htmlFor="inputContact"
-              className="form-label mandatory_field"
-            >
-              Contact Number
-            </label>
-            <input
-              ref={phoneNumberRef}
-              type="phone"
-              className="form-control"
-              id="inputContact"
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="inputEmail" className="form-label mandatory_field">
-              Email
-            </label>
-            <input
-              ref={emailRef}
-              type="email"
-              className="form-control"
-              id="inputEmail"
-              defaultValue={currentUser?.email}
-              readOnly
-            />
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="extraInfo" className="form-label">
-              Order Note(Optional)
-            </label>
-            <textarea
-              ref={extra_informationRef}
-              className="form-control"
-              id="extraInfo"
-              rows="3"
-            ></textarea>
-          </div>
-
-          <select
-            className="form-select"
-            aria-label="Default select example"
-            ref={PaymentRef}
-            required
+      <Header />
+      <main className="container-fluid catering-checkout-page">
+        <section>
+          <form
+            className="checkout_body container"
+            onSubmit={handleConfirmOrder}
           >
-            <option value="">Select Payment Methods</option>
-            <option value="Online Payment">Online Payment</option>
-            <option value="Cash on delivery">Cash on delivery</option>
-          </select>
-          <br />
-          <br />
-          <button
-            type="submit"
-            className="btn MyBtn placeOrder_btn"
-            disabled={food.length === 0}
-          >
-            {paymnetLoading ? (
-              <div className="d-flex justify-content-center">
-                <div className="spinner-border" role="status">
-                  <span className="visually-hidden">Loading...</span>
-                </div>
+            <aside className="billing_form">
+              <h5>Shipping Address</h5>
+              <div className="mb-3">
+                <label
+                  htmlFor="inputName"
+                  className="form-label mandatory_field"
+                >
+                  Your name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="inputName"
+                  defaultValue={currentUser?.displayName}
+                  readOnly
+                />
               </div>
-            ) : (
-              "Confirm Order"
-            )}
-          </button>
-        </aside>
-      </form>
+              <div className="mb-3">
+                <label htmlFor="area" className="form-label mandatory_field">
+                  Area
+                </label>
+                <select
+                  className="form-select"
+                  aria-label="Default select example"
+                  ref={cityRef}
+                  required
+                >
+                  <option value="">Select Your Area</option>
+                  <option value="Azimpur">Azimpur</option>
+                  <option value="Chankharpul">Chankharpul</option>
+                  <option value="Dhaka University Hall">
+                    Dhaka University Hall
+                  </option>
+                  <option value="Darussalam">Darussalam</option>
+                  <option value="Elephent Road">Elephent Road</option>
+                  <option value="Farmgate">Farmgate</option>
+                  <option value="Gulistan">Gulistan</option>
+                  <option value="Green Road">Green Road</option>
+                  <option value="Kola Bagan">Kola Bagan</option>
+                  <option value="Kamrangirchar">Kamrangirchar</option>
+                  <option value="Kallyanpur">Kallyanpur</option>
+                  <option value="Mohammadpur Bus Stand">
+                    Mohammadpur Bus Stand
+                  </option>
+                  <option value="Mohammadpur beribadh">
+                    Mohammadpur beribadh
+                  </option>
+                  <option value="Mohammadpur Adabor">Mohammadpur Adabor</option>
+                  <option value="Mohammadpur Asad Gate">
+                    Mohammadpur Asad Gate
+                  </option>
+                  <option value="Mirpur-1">Mirpur-1</option>
+                  <option value="New Market, Dhaka">New Market, Dhaka</option>
+                  <option value="Nakhalpara West Tejgaon">
+                    Nakhalpara West Tejgaon
+                  </option>
+                  <option value="Nakhalpara south">Nakhalpara south</option>
+                  <option value="Shahbag">Shahbag</option>
+                  <option value="Shyamoli-1">Shyamoli-1</option>
+                  <option value="Shyamoli-2">Shyamoli-2</option>
+                  <option value="Panthopath Chattor">Panthopath Chattor</option>
+                  <option value="Polashi">Polashi</option>
+                  <option value="Puran Dhaka(Bangshal)">
+                    Puran Dhaka(Bangshal)
+                  </option>
+                  <option value="Puran Dhaka (Dholaikhal)">
+                    Puran Dhaka (Dholaikhal)
+                  </option>
+                  <option value="Rayer Bazaar">Rayer Bazaar</option>
+                </select>
+              </div>
+              <div className="mb-3">
+                <label
+                  htmlFor="inputAddress"
+                  className="form-label mandatory_field"
+                >
+                  Details Address
+                </label>
+                <input
+                  ref={addressRef}
+                  type="text"
+                  className="form-control"
+                  id="inputAddress"
+                  required
+                />
+              </div>
 
-      <div>
-        {food &&
-          food.length &&
-          food?.map((fd, index) => (
-            <h1 key={index}>
-              {fd?.day}: {fd?.package}------------
-              {new Intl.NumberFormat("bn-BD").format(fd.tk)} টাকা
-            </h1>
-          ))}
-        <h1>মোট বিল: {new Intl.NumberFormat("bn-BD").format(totalTk)} টাকা</h1>
-      </div>
+              <div className="mb-3">
+                <label
+                  htmlFor="inputContact"
+                  className="form-label mandatory_field"
+                >
+                  Contact Number
+                </label>
+                <input
+                  ref={phoneNumberRef}
+                  type="phone"
+                  className="form-control"
+                  id="inputContact"
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <label
+                  htmlFor="inputEmail"
+                  className="form-label mandatory_field"
+                >
+                  Email
+                </label>
+                <input
+                  ref={emailRef}
+                  type="email"
+                  className="form-control"
+                  id="inputEmail"
+                  defaultValue={currentUser?.email}
+                  readOnly
+                />
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="extraInfo" className="form-label">
+                  Order Note(Optional)
+                </label>
+                <textarea
+                  ref={extra_informationRef}
+                  className="form-control"
+                  id="extraInfo"
+                  rows="3"
+                ></textarea>
+              </div>
+
+              <select
+                className="form-select"
+                aria-label="Default select example"
+                ref={PaymentRef}
+                required
+              >
+                <option value="">Select Payment Methods</option>
+                <option value="Online Payment">Online Payment</option>
+                <option value="Cash on delivery">Cash on delivery</option>
+              </select>
+              <br />
+              <br />
+              <button
+                type="submit"
+                className="btn MyBtn placeOrder_btn"
+                disabled={food.length === 0}
+              >
+                {paymnetLoading ? (
+                  <div className="d-flex justify-content-center">
+                    <div className="spinner-border" role="status">
+                      <span className="visually-hidden">Loading...</span>
+                    </div>
+                  </div>
+                ) : (
+                  "Confirm Order"
+                )}
+              </button>
+            </aside>
+          </form>
+        </section>
+        <section>
+          <table className="table catering-table">
+            <thead>
+              <tr>
+                <th scope="col">Days</th>
+                <th scope="col">Package</th>
+                <th scope="col">Amount</th>
+              </tr>
+            </thead>
+            <tbody>
+              {food &&
+                food.length &&
+                food?.map((fd, index) => (
+                  <tr key={index}>
+                    <td>
+                      {fd?.day}, {fd?.selectedDay} {fd?.selectedMonth}
+                    </td>
+                    <td>{fd?.package}</td>
+                    <td>{new Intl.NumberFormat("bn-BD").format(fd.tk)} টাকা</td>
+                  </tr>
+                ))}
+            </tbody>
+            <tfoot>
+              <tr>
+                <td colspan="3">
+                  <strong>মোট বিল: </strong>
+                  {new Intl.NumberFormat("bn-BD").format(totalTk)} টাকা
+                </td>
+              </tr>
+            </tfoot>
+          </table>
+        </section>
+      </main>
+      <Footer />
     </>
   );
 };
