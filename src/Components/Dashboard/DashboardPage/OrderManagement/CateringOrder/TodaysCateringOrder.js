b9a2ef4cadf9ec2f103c5eb60d0dc2c9a126aa24
unknown <za0162hid@gmail.com>
2023-08-31 16:22:22 +0600

    before two days order

--- src/Components/Dashboard/DashboardPage/OrderManagement/CateringOrder/TodaysCateringOrder.js
+++ src/Components/Dashboard/DashboardPage/OrderManagement/CateringOrder/TodaysCateringOrder.js
@@ -4,6 +4,19 @@ import axios from "axios";
 import TopbarNav from "./../../../Layouts/TopbarNav";
 import SidebarNav from "./../../../Layouts/SidebarNav";
 
+const getBeforeDays = (numDays) => {
+  const days = [];
+  const today = new Date();
+
+  for (let i = -2; i <= numDays; i++) {
+    const nextDay = new Date(today);
+    nextDay.setDate(today.getDate() + i);
+    days.push(nextDay);
+  }
+
+  return days;
+};
+
 const formatDate = (date) => {
   const banglaLocale = "bn-BD";
 
@@ -11,7 +24,7 @@ const formatDate = (date) => {
     weekday: "long",
     day: "numeric",
     month: "long",
-    year: "year",
+    year: "numeric",
   };
 
   const formattedDate = new IntlMessageFormat(
@@ -35,23 +48,18 @@ const formatDate = (date) => {
 };
 
 const TodaysCateringOrder = () => {
-  const [todayDate, setTodayDate] = useState(null);
+  const [dates, setDates] = useState(getBeforeDays(6));
+  const [selectedDate, setSelectedDate] = useState(null);
   const [todaysOrder, setTodaysOrder] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    const today = new Date();
-    const formatted = formatDate(today);
-    setTodayDate(formatted);
-  }, []);
-
   //get the todays order from backend
-  const fatchProductAccordingDate = async () => {
+  const fatchProductAccordingDate = async (selectedDate) => {
     setIsLoading(true);
     try {
       const response = await fetch(
         `${process.env.REACT_APP_BACKEND_URL}/sendTodaysDateData?todayDate=` +
-          encodeURIComponent(JSON.stringify(todayDate))
+          encodeURIComponent(JSON.stringify(selectedDate))
       );
       const data = await response.json();
       setTodaysOrder(data);
@@ -72,17 +80,23 @@ const TodaysCateringOrder = () => {
             <div className="container-fluid p-4">
               <div>
                 {isLoading && <p>Please wait...</p>}
-                <h3>
-                  - {todayDate?.weekday}, {todayDate?.day} {todayDate?.month}{" "}
-                  {todayDate?.year}
-                </h3>
+                {/* daynamic button */}
+                <section className="package-select-buttons">
+                  {dates.map((date, index) => {
+                    const formatted = formatDate(date);
 
-                <button
-                  className="btn MyBtn mt-2"
-                  onClick={() => fatchProductAccordingDate()}
-                >
-                  View Today's Catering Orders
-                </button>
+                    return (
+                      <button
+                        key={index}
+                        onClick={() => fatchProductAccordingDate(formatted)}
+                        type="button"
+                      >
+                        {formatted.weekday},{formatted.day} {formatted.month}{" "}
+                        {formatted.year}
+                      </button>
+                    );
+                  })}
+                </section>
 
                 <table className="table todays-order-table">
                   <thead>
@@ -107,12 +121,11 @@ const TodaysCateringOrder = () => {
                         <td>{todayOrderDt.cus_phone}</td>
                         {JSON.parse(todayOrderDt.food).map(
                           (foodItem, foodIndex) => (
-                            <>
-                              <span key={foodIndex}>
-                                <td>{foodItem.package}</td>
-                              </span>
+                            <div key={foodIndex}>
+                              <td>{foodItem.package}</td>
+
                               <td>{foodItem.tk} Tk.</td>
-                            </>
+                            </div>
                           )
                         )}
                         <td className="d-flex justify-content-around">
